f31ac49f8e439464f8052e76536767c9e706bcea
vyom chandra gallani <vyomchandra@gmail.com>
2020-07-12 15:57:40 +0530

    admin and authenticated checker added

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -75,3 +75,20 @@ exports.isSignedIn=expressJwt({
     algorithms: ['HS256'] 
 })
 //CUSTOM MIDDLEWARE
+exports.isAuthenticated=(req,res,next)=>{
+    let checker=req.profile && req.auth && req.profile._id===req.auth._id;
+    if(!checker){
+        return res.status(403).json({
+            error:"ACCESS DENIED "
+        })
+    }
+    next();
+}
+exports.isAdmin=(req,res,next)=>{
+    if(req.profile.role===0){
+        return res.status(403).json({
+            error:"You are not ADMIN,Access Denied",
+        })
+    }
+    next();
+}
