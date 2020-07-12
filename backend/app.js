25898b63c23ce2aa9610204f7b430799a4da8ee7
vyom chandra gallani <vyomchandra@gmail.com>
2020-07-12 10:21:36 +0530

    signup route and controller set

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,14 @@ require("dotenv").config();
 const mongoose=require('mongoose');
 const express=require("express");
 const app=express();
+const bodyParser=require("body-parser");
+const cookieParser=require("cookie-parser");
+const cors=require("cors");
 
+//MY ROUTES
+const authRoutes=require("./routes/auth");
 
+//DATABASE CONNECTIVITY
 mongoose.connect(process.env.DATABASE,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
@@ -12,8 +18,17 @@ mongoose.connect(process.env.DATABASE,{
     console.log("DB CONNECTED")
 })
 
+// MIDDLE WARE
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
+//ROUTES
+app.use("/api",authRoutes);
+
+//PORT
 const port=process.env.PORT||8000;
+//STARTING OF SERVER
 app.listen(port,()=>{
     console.log(`App is running at ${port}`);
 });
