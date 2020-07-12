e93b7721c4c7fc3cec52bdbc7ceb33f44e0127b5
vyom chandra gallani <vyomchandra@gmail.com>
2020-07-12 20:55:27 +0530

    update user done

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -20,3 +20,31 @@ exports.getUser=(req,res)=>{
     return res.json(req.profile);
 }
 
+exports.updateUser = (req, res) => {
+  User.findByIdAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true, useFindAndModify: false },
+    (err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: "You are not authorized to update this user"
+        });
+      }
+      user.salt = undefined;
+      user.encry_password = undefined;
+      res.json(user);
+    }
+  );
+};
+// exports.getAllUsers=(req,res)=>{
+//     User.find().exec((err,users)=>{
+//         if(err||!users){
+//             return res.status(400).json({
+//                 error :"No users found"
+//             })
+//         }
+//         res.json(users);
+//     })
+// }
+
