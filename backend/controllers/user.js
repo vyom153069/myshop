91e025d4399ef48f2ac35dcfd434e6822bebadf4
vyom chandra gallani <vyomchandra@gmail.com>
2020-07-12 21:01:59 +0530

    added user purchase list

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
 const User=require("../models/user");
+const Order = require("../models/order");
 
 exports.getUserById=(req,res,next,id)=>{
     User.findById(id).exec((err,user)=>{
@@ -36,6 +37,18 @@ exports.updateUser = (req, res) => {
       res.json(user);
     }
   );
+};
+exports.userPurchaseList = (req, res) => {
+  Order.find({ user: req.profile._id })
+    .populate("user", "_id name")
+    .exec((err, order) => {
+      if (err) {
+        return res.status(400).json({
+          error: "No Order in this account"
+        });
+      }
+      return res.json(order);
+    });
 };
 // exports.getAllUsers=(req,res)=>{
 //     User.find().exec((err,users)=>{
