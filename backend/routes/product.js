5ed1721f9f36ed9ae9b695e49cad5461f71ba692
vyom chandra gallani <vyomchandra@gmail.com>
2020-07-14 09:45:48 +0530

    product functionalities , delete update saving etc done

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,17 +1,60 @@
 const { request } = require("express");
 
-const express=require("express");
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
 
-const {getProductById,creatProduct}=require("../controllers/product");
-const { isSignedIn,isAuthenticated,isAdmin }=require("../controllers/auth");
-const {getUserById}=require("../controllers/user");
+const {
+  getProductById,
+  createProduct,
+  getProduct,
+  photo,
+  updateProduct,
+  deleteProduct,
+  getAllProducts,
+  getAllUniqueCategories
+} = require("../controllers/product");
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
+const { getUserById } = require("../controllers/user");
 
-//ALL OF PARAMS
-router.param("userId",getUserById);
-router.param("productId",getProductById);
+//all of params
+router.param("userId", getUserById);
+router.param("productId", getProductById);
 
-//ALL OF ACTUAL ROUTES
-router.post("/product/create/:userId",isSignedIn,isAuthenticated,isAdmin,creatProduct);
+//all of actual routes
+//create route
+router.post(
+  "/product/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createProduct
+);
 
-module.exports=router;
+// read routes
+router.get("/product/:productId", getProduct);
+router.get("/product/photo/:productId", photo);
+
+//delete route
+router.delete(
+  "/product/:productId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  deleteProduct
+);
+
+//update route
+router.put(
+  "/product/:productId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateProduct
+);
+
+//listing route
+router.get("/products", getAllProducts);
+
+router.get("/products/categories", getAllUniqueCategories);
+
+module.exports = router;
