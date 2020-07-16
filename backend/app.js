92eaaa156df93d1eac32e5babdc86167391f7d2a
vyom chandra gallani <vyomchandra@gmail.com>
2020-07-16 17:05:47 +0530

    order and product functionality done

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,8 @@ const authRoutes=require("./routes/auth");
 const userRoutes=require("./routes/user");
 const categoryRoutes=require("./routes/category");
 const productRoutes=require("./routes/product");
+const orderRoutes = require("./routes/order");
+const paymentBRoutes = require("./routes/paymentBRoutes");
 
 
 //DATABASE CONNECTIVITY
@@ -32,6 +34,8 @@ app.use("/api",authRoutes);
 app.use("/api",userRoutes);
 app.use("/api",categoryRoutes);
 app.use("/api",productRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", paymentBRoutes);
 
 //PORT
 const port=process.env.PORT||8000;
